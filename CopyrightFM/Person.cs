152894e7571d0589a3a440646540f6d7ca04a958
JohnMB97 <jhnbalzer2@gmail.com>
2018-07-01 20:09:06 -0500

    person class

---
+++ CopyrightFM/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyrightFM
+{
+    class Person
+    {
+        public string FirstName
+        {
+            get;
+        }
+
+        public string Lastname
+        {
+            get;
+        }
+        
+        public int Age
+        {
+            get;
+        }
+    }
+}
