dcb5e740ce7a2c16eb5bd4711c07ea89f4a275da
JohnMB97 <jhnbalzer2@gmail.com>
2018-08-12 11:54:50 -0500

    s

--- CopyrightFM/DataAccess.cs
+++ CopyrightFM/DataAccess.cs
@@ -30,14 +30,37 @@ namespace CopyrightFM
 
         public void AddUser(string firstName, string lastName, string orderID, string date, string initial, string phoneNum)
         {
-            
-            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CNN("CustomerApplicationDB")))
+
+            using (SqlConnection conn = new SqlConnection(Helper.CNN("CustomerApplicationDB")))
             {
-                //Person newCustomer = new Person { FirstName = firstName, Lastname = lastName, OrderID = orderID, DateS = date, Initial = initial, PhoneNum = phoneNum };
-                List<Person> customer = new List<Person>();
-                customer.Add(new Person { FirstName = firstName, Lastname = lastName, OrderID = Int64.Parse(orderID), DateS = date, Initial = initial, PhoneNum = Int64.Parse(phoneNum) });
-                connection.Execute("dbo.People_Insert @FirstName, @LastName, @OrderID, @DateS, @Initial, @PhoneNum", customer);
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Customer (FirstName, LastName, OrderID, DateS, Initial, PhoneNum) VALUES ( @FirstName, @LastName, @OrderID, @DateS, @Initial, @PhoneNum)", conn);
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+
+
+                cmd.Parameters.AddWithValue("@LastName", lastName);
+                cmd.Parameters.AddWithValue("@FirstName", firstName);
+                cmd.Parameters.AddWithValue("@OrderID", orderID);
+                cmd.Parameters.AddWithValue("@DateS", date);
+                cmd.Parameters.AddWithValue("@Initial", initial);
+                cmd.Parameters.AddWithValue("@PhoneNum", phoneNum);
+
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
             }
+
+            //using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CNN("CustomerApplicationDB")))
+            
+                //Person newCustomer = new Person { FirstName = firstName, Lastname = lastName, OrderID = orderID, DateS = date, Initial = initial, PhoneNum = phoneNum };
+                //List<Person> customer = new List<Person>();
+                //customer.Add(new Person { FirstName = firstName, Lastname = lastName, OrderID = Int64.Parse(orderID), DateS = date, Initial = initial, PhoneNum = Int64.Parse(phoneNum) });
+                //connection.Execute("dbo.People_Insert @FirstName, @LastName, @OrderID, @DateS, @Initial, @PhoneNum", customer);
+
+                
+            
         }
 
 
